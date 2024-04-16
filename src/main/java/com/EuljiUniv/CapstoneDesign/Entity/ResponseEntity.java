ed2c2291510ad6c6683bead427a5d2e327738c8f
ILikeCLoudy <wjdduq0719@naver.com>
2024-04-16 16:38:28 +0900

    second commit

--- src/main/java/com/EuljiUniv/CapstoneDesign/Entity/ResponseEntity.java
+++ src/main/java/com/EuljiUniv/CapstoneDesign/Entity/ResponseEntity.java
@@ -17,17 +17,76 @@ public class ResponseEntity {
     private String Person;
 
     @Column
-    private String Sex;
-
-    @Column
     private String StudentID;
 
     @Column
     private String Major;
 
     @Column
+    private String Sex;
+
+    @Column
     private String MBTI;
 
     @Column
-    private String first_last;
+    private String Bigdata1;
+    @Column
+    private String Bigdata2;
+    @Column
+    private String Bigdata3;
+    @Column
+    private String Bigdata4;
+    @Column
+    private String Bigdata5;
+    @Column
+    private String Bigdata6;
+    @Column
+    private String Bigdata7;
+    @Column
+    private String Bigdata8;
+    @Column
+    private String Bigdata9;
+
+    @Column
+    private String MedicalMG1;
+    @Column
+    private String MedicalMG2;
+    @Column
+    private String MedicalMG3;
+    @Column
+    private String MedicalMG4;
+    @Column
+    private String MedicalMG5;
+    @Column
+    private String MedicalMG6;
+    @Column
+    private String MedicalMG7;
+    @Column
+    private String MedicalMG8;
+    @Column
+    private String MedicalMG9;
+
+    @Column
+    private String MedicalEG1;
+    @Column
+    private String MedicalEG2;
+    @Column
+    private String MedicalEG3;
+    @Column
+    private String MedicalEG4;
+    @Column
+    private String MedicalEG5;
+    @Column
+    private String MedicalEG6;
+    @Column
+    private String MedicalEG7;
+    @Column
+    private String MedicalEG8;
+    @Column
+    private String MedicalEG9;
+
+
+
+
+
 }
