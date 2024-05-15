ebd2c1ccdc056542407dbcb14b97e839b9f7e301
ILikeCLoudy <wjdduq0719@naver.com>
2024-05-15 14:35:24 +0900

    4th-change

--- src/main/java/com/EuljiUniv/CapstoneDesign/Repository/ResponseRepository.java
+++ src/main/java/com/EuljiUniv/CapstoneDesign/Repository/ResponseRepository.java
@@ -1,15 +1,11 @@
 package com.EuljiUniv.CapstoneDesign.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.ResponseEntity;
 
-import jakarta.persistence.criteria.From;
-import java.util.List;
 import java.util.Optional;
 
 public interface ResponseRepository extends JpaRepository<ResponseEntity, Long> {
-    @Query(value = "select * from answer as a order by ID", nativeQuery = true)
-    public ResponseEntity findBy();
+    //@Query(value = "select * from answer as a order by ID", nativeQuery = true)
+    public Optional<ResponseEntity> findById(Long ID);
 }
